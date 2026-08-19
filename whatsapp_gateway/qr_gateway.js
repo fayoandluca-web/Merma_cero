25eab8efffeb35ae57f7d2b985a4c1b02ae5ae7c
Fabio Rios <fayo.and.luca@gmail.com>
2026-08-18 23:43:32 -0600

    feat: switch to pairing code method for production linking to support single-phone setup

--- whatsapp_gateway/qr_gateway.js
+++ whatsapp_gateway/qr_gateway.js
@@ -129,7 +129,9 @@ const client = new Client({
     userAgent: isLocal ? 
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' : 
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    pairWithPhoneNumber: undefined
+    pairWithPhoneNumber: isLocal ? undefined : {
+        phoneNumber: '525575049383'
+    }
 });
 
 client.on('code', (code) => {
